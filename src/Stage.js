7ce3a0df3dff14057c6f662864dd51bed75181c0
mconintet <mconintet@163.com>
2015-04-14 20:17:04 +0800

    support children tpl

--- src/Stage.js
+++ src/Stage.js
@@ -30,11 +30,15 @@ define({
             ]);
 
             this._autoAbortTasks = [];
+
+            this._tmpDiv = document.createElement('div');
+            this._childrenTpl = {};
         };
 
         sop.extendProto(Stage, sop.Observable);
 
         Stage.prototype._init = function () {
+            this._processChildrenTpl();
             this._layout = new Tpl(this.layout, this.debug).getCTpl();
             this.on('afterHide', this._destroy.bind(this));
         };
@@ -50,7 +54,43 @@ define({
         };
 
         /**
-         * Initializes stage, it will be called automatically once stage has been registered by `sop.App.registerStage`
+         * Gets rendered string of child template
+         *
+         * @param name The name of child template
+         * @param {Object} [context=sop.Message] The context to be used in render, default is the instance of `sop.Message`
+         * being used in parent stage.
+         * @returns {String} Rendered string
+         */
+        Stage.prototype.childTpl = function (name, context) {
+            context = context || this.getMessage();
+            var tpl = this._childrenTpl[name];
+            if (!tpl) {
+                throw new Error('try to retrieve child tpl with a unknown name: ' + name + ' of stage:' + this.route);
+            }
+
+            return tpl.render(context, this);
+        };
+
+        Stage.prototype._processChildrenTpl = function () {
+            this._tmpDiv.innerHTML = this.layout;
+
+            var me = this;
+            sop.$all('script[type="text/jsTpl"]', this._tmpDiv).forEach(function (e) {
+                var n = e.dataset.name;
+                if (!n) {
+                    throw new Error('there muse be a name of child tpl, stage: ' + me.route);
+                }
+
+                me._childrenTpl[n] = new Tpl(e.innerHTML, me.debug).getCTpl();
+                me._tmpDiv.removeChild(e);
+            });
+
+            this.layout = this._tmpDiv.innerHTML;
+            this._tmpDiv.innerHTML = null;
+        };
+
+        /**
+         * Initializes stage, it will be called automatically by `sop.App`
          */
         Stage.prototype.init = function () {
             if (this.isReady)
@@ -82,15 +122,16 @@ define({
         };
 
         /**
-         * Get rendered html of stage, it will be called automatically when stages are interacting
+         * Gets rendered html of stage, it will be called automatically when stages are interacting
          *
-         * @param context {Object} Context within render
+         * @param [context=sop.Message] {Object} The context to be used in render, default is the instance of `sop.Message`
+         * being used in current stage
          * @returns {String} Rendered html
          */
         Stage.prototype.render = function (context) {
             var html;
             this.fire('beforeRender');
-            html = this._layout.render(context);
+            html = this._layout.render(context || this.getMessage(), this);
             this.fire('afterRender');
 
             return html;
@@ -106,7 +147,7 @@ define({
         };
 
         /**
-         * Add auto-abort task, auto-abort task will be abort automatically if it's still running after
+         * Adds auto-abort task, auto-abort task will be abort automatically if it's still running after
          * it's associative stage has been hidden
          *
          * @param task
